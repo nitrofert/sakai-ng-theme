5730e6175020054322683ef23f1217f5e38d3051
Ronald Albor <ralbor@nitrofert.com.co>
2023-08-29 13:32:41 -0500

    test url backend3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   env:'prod',
   //url_backend:'http://backend-autogestion.nitrofert.com.co',
-  url_backend:'https://fixed-packages.d39j6qsi811pio.amplifyapp.com/',
+  url_backend:'https://fixed-packages.d39j6qsi811pio.amplifyapp.com',
   companySAP:'NITROFERT_PRD',
   companyMySQL:'nitrosap'
 };
