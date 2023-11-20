f74d261646db15533aaff60f0b28b2f0a22a93dd
Ronald Albor <ralbor@nitrofert.com.co>
2023-11-20 14:19:00 -0500

    correccion manejo de ambientes app

--- src/environments/environment.calidad.ts
+++ src/environments/environment.calidad.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     env:'calidad',
-    url_backend:'http://backend_test-autogestion.nitrofert.com.co',
+    url_backend:'http://backend-test-autogestion.nitrofert.com.co',
     url_backend_ssl:'https://backend-test-autogestion.nitrofert.com.co',
     companySAP:'NITROFERT_PRD',
     companyMySQL:'nitrosap'
