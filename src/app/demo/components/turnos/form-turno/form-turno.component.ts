e245d7aa2966c59022ec22c13b0b2e7f7334c69e
Ronald Javier Albor Cardenas <ralbor@nitrofert.com.co>
2025-08-05 15:03:28 -0500

    quitar validacion de pesos

--- src/app/demo/components/turnos/form-turno/form-turno.component.ts
+++ src/app/demo/components/turnos/form-turno/form-turno.component.ts
@@ -1940,11 +1940,12 @@ async validarHoraCargue():Promise<boolean>{
     //console.log('this.estado_turno_actual',estado_turno_actual )
     //console.log('this.estado',this.estado )
      
+    
 
-    if((this.accion != 'pausar' &&  this.accion != 'cancelar' ) &&  estado_turno_actual == this.estadosTurno.AUTORIZADO && this.turno.conductor.historial_arl.length===0 && (this.turno.tipo ==='RETIRO' || (this.turno.tipo ==='ENTREGA' && this.turno.detalle_solicitud_turnos_pedido.filter((pedido: { tipo_operacion: string; })=>pedido.tipo_operacion==='ENTREGA').length >0 ))){
+    if((this.accion != 'pausar' &&  this.accion != 'cancelar' &&  this.accion != 'actualizar la información del turno y reestablecer el estado  de ') &&  estado_turno_actual == this.estadosTurno.AUTORIZADO && this.turno.conductor.historial_arl.length===0 && (this.turno.tipo ==='RETIRO' || (this.turno.tipo ==='ENTREGA' && this.turno.detalle_solicitud_turnos_pedido.filter((pedido: { tipo_operacion: string; })=>pedido.tipo_operacion==='ENTREGA').length >0 ))){
       this.messageService.add({severity:'error', summary: '!Error¡', detail: `El conductor ${this.turno.conductor.nombre} no tiene asociado una ARL` });
       error = true;
-    }else if((this.accion != 'pausar' &&  this.accion != 'cancelar' ) && estado_turno_actual == this.estadosTurno.AUTORIZADO && this.hoy > fecha_vigencia_arl_activa && (this.turno.tipo ==='RETIRO' || (this.turno.tipo ==='ENTREGA' && this.turno.detalle_solicitud_turnos_pedido.filter((pedido: { tipo_operacion: string; })=>pedido.tipo_operacion==='ENTREGA').length >0 ))){
+    }else if((this.accion != 'pausar' &&  this.accion != 'cancelar' &&  this.accion != 'actualizar la información del turno y reestablecer el estado  de ') && estado_turno_actual == this.estadosTurno.AUTORIZADO && this.hoy > fecha_vigencia_arl_activa && (this.turno.tipo ==='RETIRO' || (this.turno.tipo ==='ENTREGA' && this.turno.detalle_solicitud_turnos_pedido.filter((pedido: { tipo_operacion: string; })=>pedido.tipo_operacion==='ENTREGA').length >0 ))){
       this.messageService.add({severity:'error', summary: '!Error¡', detail: `La fecha de vigencia de la ARL del conductor ${fecha_vigencia_arl_activa.toISOString().split('T')[0]} es menor a la fecha actual  ${hoy.toISOString().split('T')[0]} ` });
       error = true;
     }
