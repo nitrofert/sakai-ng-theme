ba7fcb72766e121e889ffda3d288fadd0bbf3a23
Ronald Javier Albor Cardenas <ralbor@nitrofert.com.co>
2025-09-22 14:54:22 -0500

    cambio de ssl en ambiente calidad

--- src/environments/environment.calidad.ts
+++ src/environments/environment.calidad.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     env:'calidad',
     url_backend:'backend-test-autogestion.nitrofert.com.co',
-    url_ssl:false,
+    url_ssl:true,
     companySAP:'PRUEBAS_NITROFERT_PRD_OCT27',
     companyMySQL:'nitrosap',
     ID_google_analytis:'',
