be557e38dc7ea7292c2e5313ebbc593bec44b303
Ronald Javier Albor Cardenas <ralbor@nitrofert.com.co>
2025-09-22 21:20:02 -0500

    cambio

--- src/app/demo/components/turnos/vista-movil-turno/turnos-movil.ts
+++ src/app/demo/components/turnos/vista-movil-turno/turnos-movil.ts
@@ -854,7 +854,7 @@ export class TurnosMovilComponent implements OnInit, AfterViewInit {
         this.messageService.add({severity:'error', summary: '!Error¡', detail: 'Debe asignar lotes de producccion para cada item de producto-destino.'});
     }else if(this.estado === this.estadosTurno.CARGANDO && this.pedidos_turno.filter(pedido=>pedido.maneja_lote==='N' && !pedido.cantidad_sacos ).length>0){
         this.messageService.add({severity:'error', summary: '!Error¡', detail: 'Debe asignar la cantidad de sacos a cargar para todas las lineas de pedido del turno, y esta debe ser mayor a cero.'});
-    }else if(this.estado === this.estadosTurno.CARGANDO && this.pedidos_turno.filter(pedido=>this.pedidos_turno.filter(pedido=>pedido.cubicacion ==="")).length>0){
+    }else if(this.estado === this.estadosTurno.CARGANDO && this.pedidos_turno.filter(pedido=>pedido.cubicacion ==="").length>0){
         this.messageService.add({severity:'error', summary: '!Error¡', detail: 'Debe ingresar la cubicacion para las lineas de items.'});
     }else if(this.estado === this.estadosTurno.CARGANDO && await this.validarEvidenciasItemPedido()=== false){
            this.messageService.add({severity:'error', summary: '!Error¡', detail: `No se han adjuntado evidencias del cargue`});
